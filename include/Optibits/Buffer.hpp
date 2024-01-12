af154925c154c6cb734f5107b71d2aafac64b592
tourahi <amine-fb-fb@hotmail.fr>
2024-01-12 02:55:47 +0100

    Buffer construct & PlatformHPP.

--- include/Optibits/Buffer.hpp
+++ include/Optibits/Buffer.hpp
@@ -13,13 +13,30 @@ namespace Optibits
   {
     void* m_buffer;
     std::size_t m_size;
-    std::function<void(void*)> m_purge;
+    std::function<void(void*)> m_purger;
 
   public:
 
     Buffer() = default;
 
     Buffer(void* buffer, std::size_t size, std::function<void(void*)> purger);
+    Buffer(Buffer&& other) noexcept;
+    Buffer& operator=(Buffer&& other) noexcept;
+    ~Buffer();
 
+    explicit Buffer(std::size_t size);
+    explicit Buffer(std::vector<std::uint8_t>&& vector) noexcept; 
+
+    Buffer(const Buffer& other);
+    Buffer& operator=(const Buffer& other);
+
+    void drop();
+    const std::uint8_t* data() const { return static_cast<const std::uint8_t*>(m_buffer); }
+    std::uint8_t* data() { return static_cast<std::uint8_t*>(m_buffer); }
+    std::size_t size() const { return m_size; }
   };
+
+  Buffer load_file(const std::string& filename);
+
+
 }
--- src/Buffer.cpp
+++ src/Buffer.cpp
@@ -1,5 +1,4 @@
 #include <Optibits/Buffer.hpp>
-
 #include <limits>
 #include <memory>
 #include <stdexcept>
@@ -7,15 +6,92 @@
 #include <SDL.h>
 
 
+void Optibits::Buffer::drop()
+{
+  m_buffer = nullptr;
+  m_size = 0;
+  m_buffer = nullptr;
+}
+
 Optibits::Buffer::Buffer(void* buffer, std::size_t size, std::function<void(void*)> purger)
   : m_buffer(buffer),
     m_size(size),
-    m_purge(purger)
+    m_purger(purger)
 {
   if (buffer == nullptr && size > 0) {
     throw std::invalid_argument("Tried to create non-empty Optibits::Buffer from nullptr");
   }
   if (size >= std::numeric_limits<std::ptrdiff_t>::max()) {
     throw std::invalid_argument("Tried to create Optibits::Buffer with negative size");
-  } 
+  }
 }
+
+Optibits::Buffer::Buffer(Optibits::Buffer&& other) noexcept
+  : m_buffer(nullptr),
+    m_size(0),
+    m_purger(nullptr)
+{
+  *this = std::move(other); /// Allow efficient transfer 
+}
+
+Optibits::Buffer& Optibits::Buffer::operator=(Optibits::Buffer&& other) noexcept
+{
+
+  if (this == &other) {
+    return *this;
+  }
+
+  if (m_buffer && m_purger) {
+    m_purger(m_buffer);
+  }
+
+  m_buffer = other.m_buffer;
+  m_size = other.m_size;
+  m_purger = std::move(other.m_purger);
+
+  other.drop();
+
+  return *this;
+}
+
+Optibits::Buffer::~Buffer() {
+  if (m_buffer && m_purger) {
+    m_purger(m_buffer);
+  }
+}
+
+Optibits::Buffer::Buffer(std::size_t size)
+  : m_buffer(nullptr),
+    m_size(size),
+    m_purger([](auto* p) { delete[] static_cast<std::uint8_t*>(p); })
+{
+  if (size >= std::numeric_limits<std::ptrdiff_t>::max()) {
+    throw std::length_error("Tried creating Optibitx::Buffer with a negative size.");
+  }
+
+  if (m_size) {
+    m_buffer = new std::uint8_t[m_size];
+  }
+}
+
+Optibits::Buffer::Buffer(std::vector<std::uint8_t>&& vector) noexcept
+  : m_buffer(vector.data()),
+    m_size(vector.size()),
+    m_purger([vec = std::move(vector)](void*) mutable { vec.clear(); })
+{
+}
+
+Optibits::Buffer::Buffer(const Optibits::Buffer& other)
+  : Buffer(std::vector<std::uint8_t>(other.data(), other.data() + other.size()))
+{
+}
+
+Optibits::Buffer& Optibits::Buffer:operator=(const Optibits::Buffer& other)
+[
+  if (this == &other)
+  {
+    return *this;
+  }
+
+  return *this = Buffer(other);
+]

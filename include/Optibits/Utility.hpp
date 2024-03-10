d3dfc08acbf877a5cbf2ce22f492d0b880cd3842
tourahi <amine-fb-fb@hotmail.fr>
2024-03-10 16:36:44 +0000

    Rect Utility.

--- include/Optibits/Utility.hpp
+++ include/Optibits/Utility.hpp
@@ -12,4 +12,44 @@ namespace Optibits
 
   std::vector<std::string> user_languages();
 
+  std::u32string utf8_to_composed_utc4(std::string_view utf8);
+
+
+  class Noncopyable
+  {
+  protected:
+    Noncopyable() = default;
+    ~Noncopyable() = default;
+
+  public:
+    Noncopyable(const Noncopyable& other) = delete;
+    Noncopyable& operator=(const Noncopyable& other) = delete;
+
+    Noncopyable(Noncopyable&& other) = delete;
+    Noncopyable& operator=(Noncopyable&& other) = delete;
+  };
+
+  // Int based rect. <Internal-use>
+  struct Rect
+  {
+    int x = 0, y = 0;
+    int width = 0, height = 0;  
+
+    int right() const { return x + width; }
+    int bottom() const { return y + height; }
+
+    bool empty() const { return width <= 0 || height <=  0; }
+
+    template <typename  T> static Rect  covering(const T& obj)
+    {
+      return Rect { .x = 0, .y = 0, .width = obj.width(), .height = obj.height() };
+    }
+
+    bool overlaps(const Rect& other) const;
+    bool contains(const Rect& other) const;
+
+    void clipTo(const Rect& boundingBox, int*  adjustX = nullptr, int*  adjustY = nullptr);
+    
+    bool operator==(const Rect& other) const = default;
+  };
 }
--- src/Utility.cpp
+++ src/Utility.cpp
@@ -65,3 +65,92 @@ std::vector<std::string> Optibits::user_languages()
 // TODO
 #endif
 #endif
+
+
+
+std::u32string Optibits::utf8_to_composed_utc4(std::string_view utf8)
+{
+  std::u32string utc4;
+  utc4.reserve(utf8.size());
+
+  const auto* current_byte = reinterpret_cast<const utf8proc_uint8_t*>(utf8.data());
+  auto remaining_length = static_cast<utf8proc_ssize_t>(utf8.length());
+
+  utf8proc_int32_t codepoint;
+  while (remaining_length) {
+    auto bytes_read = utf8proc_iterate(current_byte, remaining_length, &codepoint);
+    if (bytes_read < 0) {
+      // Not looking good, skip this byte and retry.
+      current_byte += 1;
+      remaining_length -= 1;
+    }
+    else {
+      utc4.push_back(codepoint);
+      current_byte += bytes_read;
+      remaining_length -= bytes_read;
+    }
+  }
+
+  // Now compose characters in-place.
+  auto* utc4_data = reinterpret_cast<utf8proc_int32_t*>(utc4.data());
+  auto utc4_length = static_cast<utf8proc_ssize_t>(utc4.length());
+  auto options = static_cast<utf8proc_option_t>(UTF8PROC_NLF2LF | UTF8PROC_COMPOSE);
+  auto new_length = utf8proc_normalize_utf32(utc4_data, utc4_length, options);
+  // GCOV_EXCL_START: No code path in utf8proc_normalize_utf32 currently returns an error.
+  if (new_length < 0) {
+    throw std::runtime_error("Could not normalize '" + std::string(utf8)
+      + "': " + utf8proc_errmsg(new_length));
+  }
+  // GCOV_EXCL_END
+  utc4.resize(new_length);
+
+  return utc4;
+
+}
+
+bool Optibits::Rect::overlaps(const Rect& other) const
+{
+  return x < other.right() && other.x < right() && y < other.bottom() && other.y < bottom();
+}
+
+bool Optibits::Rect::contains(const Rect& other) const
+{
+  return x <= other.x && other.right() <= right() && y <= other.y && other.bottom() <= bottom();
+}
+
+void Optibits::Rect::clipTo(const Rect& boundingBox, int*  adjustX, int*  adjustY)
+{
+
+  const int extraPixelsLeft = boundingBox.x - x;
+  if (extraPixelsLeft) {
+    x += extraPixelsLeft;
+    width -= extraPixelsLeft;
+    if (adjustX) {
+      *adjustX += extraPixelsLeft;
+    }
+  }
+
+  const int extraPixelsRight = right() - boundingBox.right();
+  if (extraPixelsRight) {
+    width -= extraPixelsRight;
+  }
+
+  const int extraPixelsTop = boundingBox.y - y;
+  if (extraPixelsTop) {
+    y += extraPixelsTop;
+    height -= extraPixelsTop;
+    if (adjustY) {
+      *adjustY += extraPixelsTop;
+    }
+  }
+
+  const int extraPixelsBottom = bottom() - boundingBox.bottom();
+  if (extraPixelsBottom) {
+    height -= extraPixelsBottom;
+  }
+
+  if (width < 0) { width = 0; }
+  if (height < 0) { height = 0; }
+}
+
+

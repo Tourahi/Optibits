2ed9abe689d6246b332ec34d4387a16dee19294a
Tourahi <amine-fb-fb@hotmail.fr>
2025-05-25 18:07:59 +0100

    [CMAKE-01] parsing the config header.

---
+++ src/config.h
@@ -0,0 +1,20 @@
+/**********************************************************************************************
+*
+*   optibits configuration flags
+*
+*   This file defines all the configuration flags for the different optibits modules
+*
+*   LICENSE: TO-DO
+*
+*   Copyright (c) 2025 tourahi
+*
+**********************************************************************************************/
+
+#ifndef CONFIG_H
+#define CONFIG_H
+
+
+
+
+
+#endif // CONFIG_H

7a32efbb045241fd4cdb32b47e6e7d8e53336b54
Tourahi <amine-fb-fb@hotmail.fr>
2025-05-31 16:04:56 +0100

    Added: Optional.

---
+++ src/common/Optional.h
@@ -0,0 +1,49 @@
+
+#ifndef OPTI_OPTIONAL_H
+#define OPTI_OPTIONAL_H
+
+namespace Optional
+{
+
+  template <typename T>
+  struct Optional
+  {
+
+    T value;
+    bool hasValue;
+
+    Optional()
+      : value(T()),
+        hasValue(false)
+    {}
+
+    Optional(T value)
+      : value(value),
+        hasValue(true)
+    {}
+
+    void set(T value)
+    {
+      value = value;
+      hasValue = true;
+    }
+
+    T get(T defaultVal) const
+    {
+      return hasValue ? value : defaultVal;
+    }
+
+    void clear()
+    {
+      hasValue = false;
+    }
+
+  };
+
+  typedef Optional<bool>   OptionalBool;
+  typedef Optional<float>  OptionalFloat;
+  typedef Optional<double> OptionalDouble;
+  typedef Optional<int>    OptionalInt;
+
+}
+#endif

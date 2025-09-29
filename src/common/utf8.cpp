7a3f599424365bd62327f174351c2aa9816aa193
tourahi <amine-fb-fb@hotmail.fr>
2025-09-30 00:07:48 +0100

    Added: Win utf8 wstring conversions.

--- src/common/utf8.cpp
+++ src/common/utf8.cpp
@@ -5,8 +5,55 @@
 namespace opti
 {
 
+  std::string to_utf8(LPCWSTR wstr) {
+    size_t wideLen = wslen(wstr) + 1;
 
+    // get size in UTF-8
+    int utf8Size = WideCharToMultiByte(CP_UTF8, 0, wstr, wideLen, 0, 0, 0, 0);
 
+    char *utf8Str = new char[utf8Size];
+
+    int ok = WideCharToMultiByte(CP_UTF8, 0, wstr, wide_len, utf8_str, utf8_size, 0, 0);
+
+    std::string ret;
+    if (ok)
+      ret = utf8_str;
+
+    delete[] utf8_str;
+    return ret;
+  }
+
+  std::wstring to_widestr(const std::string &str)
+  {
+    if (str.empty())
+      return std::wstring();
+
+    int wide_size = MultiByteToWideChar(CP_UTF8, 0, str.c_str(), (int) str.length(), nullptr, 0);
+
+    if (wide_size == 0)
+      return std::wstring();
+
+    std::wstring widestr;
+    widestr.resize(wide_size);
+
+    int ok = MultiByteToWideChar(CP_UTF8, 0, str.c_str(), (int) str.length(), &widestr[0], widestr.length());
+
+    if (!ok)
+      return std::wstring();
+
+    return widestr;
+  }
+  
+  void replace_char(std::string &str, char find, char replace)
+  {
+    int length = str.length();
+
+    for (int i = 0; i<length; i++)
+    {
+      if (str[i] == find)
+        str[i] = replace;
+    }
+  }
 }
 
 #endif
--- src/common/utf8.h
+++ src/common/utf8.h
@@ -6,6 +6,8 @@
 
 #ifdef OPTI_WINDOWS
 
+#include <string>
+
 #define WIN32_LEAN_AND_MEAN
 #include <windows.h>
 
@@ -13,10 +15,27 @@
 namespace opti
 {
 
+    /**
+     * Convert the wide string to a UTF-8 encoded string.
+     * @param wstr The wide-char string.
+     * @return A UTF-8 string.
+     **/
     std::string to_utf8(LPCWSTR wstr);
 
+    /**
+     * Convert a UTF-8 encoded string to a wide string.
+     * @param str The UTF-8 string.
+     * @return A wide string.
+     **/
     std::wstring to_widestr(const std::string &str);
 
+
+    /**
+     * Replace all occurences of 'find' with 'replace' in a string.
+     * @param str The string to modify.
+     * @param find The character to match.
+     * @param replace The character to replace matches.
+     **/
     void replace_char(std::string &str, char find, char replace);
 
 }
